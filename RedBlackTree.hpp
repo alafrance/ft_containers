0cf4486af415bf562175e285533442aa6bfbc66a
Alexis LAFRANCE <alexislafrances-laf@outlook.fr>
2021-11-24 16:09:55 +0100

    start delete and beautify the code

--- RedBlackTree.hpp
+++ RedBlackTree.hpp
@@ -28,6 +28,8 @@ struct node {
 template<typename T>
 class RBTree {
 public:
+
+	// ---------------- CANONICAL FORM ----------------
 	RBTree() : root(nullptr) {}
 
 	explicit RBTree(T data) : root(createNode(data, black)) {}
@@ -35,7 +37,7 @@ public:
 	RBTree(RBTree const &tree) : root(nullptr) { root = tree.root; }
 
 	~RBTree() {
-		deleteTree(root);
+//		deleteTree(root);
 	}
 
 	RBTree &operator=(RBTree<T> const &tree) {
@@ -46,6 +48,7 @@ public:
 		}
 		return (*this);
 	}
+	// ---------------- UTILITY COPY / CREATE ----------------
 
 	node<T> *createNode(T data) const {
 		node<T> *tmp;
@@ -69,106 +72,26 @@ public:
 		return (tmp);
 	}
 
-	void insert(node<T> *add) {
-		insert(root, add);
-	}
-
-	void display() const {
-		iter(root, &RBTree::displayNode);
-	};
-
-	void deleteTree(node<T> *node) {
-		if (node == nullptr) {
-			return;
-		}
-		deleteTree(node->left);
-		deleteTree(node->right);
-		delete node;
-	}
-
-	void leftRotate(node<T> *x) {
-		node<T> *y = x->right;
-		x->right = y->left;
-		if (y->left != nullptr) {
-			y->left->parent = x;
-		}
-		y->parent = x->parent;
-		if (x->parent == nullptr) {
-			this->root = y;
-		} else if (x == x->parent->left) {
-			x->parent->left = y;
-		} else {
-			x->parent->right = y;
-		}
-		y->left = x;
-		x->parent = y;
-	}
-
-	void rightRotate(node<T> *x) {
-		node<T> *y = x->left;
-		x->left = y->right;
-		if (y->right != nullptr) {
-			y->right->parent = x;
-		}
-		y->parent = x->parent;
-		if (x->parent == nullptr) {
-			this->root = y;
-		} else if (x == x->parent->right) {
-			x->parent->right = y;
-		} else {
-			x->parent->left = y;
-		}
-		y->right = x;
-		x->parent = y;
-	}
-
-	void rotate(node<T> *node, e_direction direction) {
-		if (node == nullptr || node->parent == nullptr)
-			return;
-		::node<T> *gp = node->parent->parent;
-		::node<T> *p = node->parent;
-		::node<T> **gpLink = nullptr;
-		if (gp)
-			gpLink = (gp->left == p) ? &gp->left : &gp->right;
-		if ((p->right == node && direction == right) ||
-			(p->left == node && direction == left)) {
-			std::cout << "wrong direction" << std::endl;
-			std::cout << "node wrong direction : " << node->data << " and direction : " << direction << std::endl;
-			displayTree();
-			return;
-		}
-		// ROTATE PARENT CHILD AND NODE CHILD LEFT
-		if (direction == left) {
-			p->right = node->left;
-			if (node->left)
-				node->left->parent = p;
-			node->left = p;
-		} else { // ROTATE PARENT CHILD AND NODE CHILD RIGHT
-			p->left = node->right;
-			if (node->right)
-				node->right->parent = p;
-			node->right = p;
-		}
-		// MODIFY POINTER OF GP AND MAKE NODE AS PARENT IF NECESSARY
-		if (gp == nullptr)
-			root = node;
-		else
-			*gpLink = node;
-		// MODIFY THE PARENT
-		p->parent = node;
-		node->parent = gp;
-	}
-
-	void displayTree() const {
-		printHelper(root, "", true);
-	}
-
+	// ---------------- GETTER  ----------------
 	node<T> *getRoot() const {
 		return root;
 	}
 
-private:
-	node<T> *root;
+	// ---------------- HELPER ----------------
+
+	void insert(T key) {
+		insert(root, createNode(key));
+	}
+
+	void displayOrder() const {
+		iter(root, &RBTree::displayNode);
+	};
+
+	void display() const {
+		printHelper(root, "", true);
+	}
+
+	// ---------------- DISPLAY ----------------
 
 	void postOrderDisplay(node<T> *node, int depth) const {
 		if (node == nullptr) {
@@ -210,59 +133,8 @@ private:
 		(this->*fct)(pRoot);
 		iter(pRoot->right, fct);
 	};
-public:
-	void insertFix(node<T> *node, bool debug) {
-		if (node == nullptr)
-			return;
-		if (node->parent == nullptr) {
-			node->color = black;
-			return;
-		}
-		if (node->parent->parent == nullptr)
-			return;
-		e_direction gp_to_p_direction;
-		while (node->parent->color == red) {
-			if (node->parent == node->parent->parent->left) // CHOOSE GPLINK DIRECTION
-				gp_to_p_direction = left;
-			else
-				gp_to_p_direction = right;
-			// FIND THE GOOD UNCLE WITH THE GPLINK DIRECTION
-			::node<T> *uncle = (gp_to_p_direction == left) ? node->parent->parent->right : node->parent->parent->left;
-			// CASE 1 UNCLE IS RED : MODIFY THE CHILD OF GRAND PARENT BLACK AND GRAND PARENT RED, AND GO TO GP
-			if (!uncle)
-				std::cout << "NO UNCLE\n";
-			if (uncle && uncle->color == red) {
-				std::cout << "Case 1 node : " << node->data << std::endl;
-				if (debug) {
-					displayTree();
-				}
-				node->parent->color = black;
-				uncle->color = black;
-				node->parent->parent->color = red;
-				node = node->parent->parent;
-			} else {
-				// CASE 2 : IF
-				if ((gp_to_p_direction == right && node == node->parent->left)
-					|| (gp_to_p_direction == left && node == node->parent->right)) {
-					::node<T> *tmp = node->parent;
-					rotate(node, gp_to_p_direction);
-					node = tmp;
-				}
-				// CASE 3
-				if (node->parent)
-					node->parent->color = black;
-				if (node->parent && node->parent->parent)
-					node->parent->parent->color = red;
-				rotate(node->parent, (gp_to_p_direction == left) ? right : left);
-			}
-			// LOOK IF WE ARE INTO ROOT OR IF WE HAVE NULLPTR
-			if (node == root || node == nullptr || node->parent == nullptr
-				|| node->parent->parent == nullptr) {
-				root->color = black; // MAKE ROOT BLACK (FIRST PROPERTIES OF R-D-TREE)
-				break;
-			}
-		}
-	}
+
+	// ---------------- SEARCH ----------------
 
 	node<T> *search_key(T data) {
 		return (search_key(root, data));
@@ -281,7 +153,7 @@ public:
 		return find;
 	}
 
-private:
+	// ---------------- INSERT  ----------------
 	void insert(node<T> *node, ::node<T> *add) {
 		if (search_key(add->data)) {
 			std::cerr << "KEY UNIQUE : " << add->data << std::endl;
@@ -297,24 +169,146 @@ private:
 			if (node->left == nullptr) {
 				node->left = add;
 				node->left->parent = node;
-				if (add->data == 45)
-					insertFix(node->left, 1);
-				else
-					insertFix(node->left, 0);
+				insertFix(node->left);
 			} else
 				insert(node->left, add);
 		} else {
 			if (node->right == nullptr) {
 				node->right = add;
 				node->right->parent = node;
-				if (add->data == 45)
-					insertFix(node->right, 1);
-				else
-					insertFix(node->right, 0);
+				insertFix(node->right);
 			} else
 				insert(node->right, add);
 		}
 	}
+
+	// ---------------- INSERT UTILITIES ----------------
+private:
+	void rotate(node<T> *node, e_direction direction) {
+		if (node == nullptr || node->parent == nullptr)
+			return;
+		::node<T> *gp = node->parent->parent;
+		::node<T> *p = node->parent;
+		::node<T> **gpLink = nullptr;
+		if (gp)
+			gpLink = (gp->left == p) ? &gp->left : &gp->right;
+		if ((p->right == node && direction == right) ||
+			(p->left == node && direction == left)) {
+			std::cerr << "wrong direction" << std::endl;
+			std::cerr << "node wrong direction : " << node->data << " and direction : " << direction << std::endl;
+			return;
+		}
+		// ROTATE PARENT CHILD AND NODE CHILD LEFT
+		if (direction == left) {
+			p->right = node->left;
+			if (node->left)
+				node->left->parent = p;
+			node->left = p;
+		} else { // ROTATE PARENT CHILD AND NODE CHILD RIGHT
+			p->left = node->right;
+			if (node->right)
+				node->right->parent = p;
+			node->right = p;
+		}
+		// MODIFY POINTER OF GP AND MAKE NODE AS PARENT IF NECESSARY
+		if (gp == nullptr)
+			root = node;
+		else
+			*gpLink = node;
+		// MODIFY THE PARENT
+		p->parent = node;
+		node->parent = gp;
+	}
+
+	void insertFix(node<T> *node) {
+		if (node == nullptr)
+			return;
+		if (node->parent == nullptr) {
+			node->color = black;
+			return;
+		}
+		if (node->parent->parent == nullptr)
+			return;
+		e_direction gp_to_p_direction;
+		while (node->parent->color == red) {
+			if (node->parent == node->parent->parent->left) // CHOOSE GPLINK DIRECTION
+				gp_to_p_direction = left;
+			else
+				gp_to_p_direction = right;
+			// FIND THE GOOD UNCLE WITH THE GPLINK DIRECTION
+			::node<T> *uncle = (gp_to_p_direction == left) ? node->parent->parent->right : node->parent->parent->left;
+			// CASE 1 UNCLE IS RED : MODIFY THE CHILD OF GRAND PARENT BLACK AND GRAND PARENT RED, AND GO TO GP
+			if (uncle && uncle->color == red) { // CASE IF UNCLE RED => COLOR BLACK PARENTS OF GP AND GP TO RED, GO TO GP
+				std::cout << "Case 1 node : " << node->data << std::endl;
+				node->parent->color = black;
+				uncle->color = black;
+				node->parent->parent->color = red;
+				node = node->parent->parent;
+			} else {
+				// NO UNCLE RED OR NO UNCLE TRY TO ROTATE IF POSSIBLE
+				if ((gp_to_p_direction == right && node == node->parent->left)
+					|| (gp_to_p_direction == left && node == node->parent->right)) {
+					::node<T> *tmp = node->parent;
+					rotate(node, gp_to_p_direction);
+					node = tmp;
+				}
+				// MODIFY PARENT COLOR AND GRAND PARENT COLOR AND ROTATE
+				if (node->parent)
+					node->parent->color = black;
+				if (node->parent && node->parent->parent)
+					node->parent->parent->color = red;
+				rotate(node->parent, (gp_to_p_direction == left) ? right : left);
+			}
+			// LOOK IF WE ARE INTO ROOT OR IF WE HAVE NULLPTR
+			if (node == root || node == nullptr || node->parent == nullptr
+				|| node->parent->parent == nullptr) {
+				root->color = black; // MAKE ROOT BLACK (FIRST PROPERTIES OF R-D-TREE)
+				break;
+			}
+		}
+	}
+
+public:
+
+	// ---------------- DELETE  ----------------
+
+	void deleteTree(node<T> *node) {
+		if (node == nullptr) {
+			return;
+		}
+		deleteTree(node->left);
+		deleteTree(node->right);
+		delete node;
+	}
+
+	void deleteNode(node<T> *node) {
+		if (node == nullptr)
+			return ;
+//		::node<T> *tmp;
+//		e_color originalColor = node->color;
+		if (node->left == NULL) {
+//			tmp = node->right;
+			transplant(node, node->right);
+			display();
+		}
+	}
+
+	// ---------------- DELETE UTILITIES  ----------------
+
+	void transplant(node<T> *uncle, node<T> *node) {
+		if (uncle == nullptr || uncle->parent == nullptr)
+			std::cerr << "Transplant no uncle or uncle invalid" << std::endl;
+		if (uncle->parent == nullptr)
+			root = node;
+		else if (uncle->parent && uncle == uncle->parent->left)
+			uncle->parent->left = node;
+		else
+			uncle->parent->right = node;
+		node->parent = uncle->parent;
+	}
+	// ---------------- ATTRIBUTES  ----------------
+private:
+	node<T> *root;
 };
 
 #endif //FT_CONTAINERS_REDBLACKTREE_HPP
--- main.cpp
+++ main.cpp
@@ -6,25 +6,13 @@
 #include "RedBlackTree.hpp"
 
 int main() {
-	RBTree<int> tree(10);
-	tree.insert(tree.createNode(13));
-	tree.insert(tree.createNode(20));
-	tree.insert(tree.createNode(30));
-	tree.insert(tree.createNode(53));
-	tree.insert(tree.createNode(11));
-	tree.insert(tree.createNode(21));
-	tree.insert(tree.createNode(41));
-	tree.insert(tree.createNode(61));
-	tree.insert(tree.createNode(15));
-	tree.insert(tree.createNode(31));
-	tree.insert(tree.createNode(42));
-	tree.insert(tree.createNode(43));
-	tree.insert(tree.createNode(56));
-	tree.insert(tree.createNode(36));
-	tree.insert(tree.createNode(27));
-	tree.insert(tree.createNode(52));
-	tree.insert(tree.createNode(45));
-	tree.insert(tree.createNode(78));
-	tree.insert(tree.createNode(100));
-	tree.displayTree();
+	RBTree<int> tree(55);
+	tree.insert(40);
+	tree.insert(65);
+	tree.insert(60);
+	tree.insert(75);
+	tree.insert(57);
+	tree.display();
+	tree.deleteTree(tree.getRoot()->left);
+	// DELETE TREE UNCOMMENT BE CAREFUL
 }
